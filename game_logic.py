eb161e19893811ef641f96885eccd7a7a1e967a3
EnglandAgain <zkoenigxd@gmail.com>
2024-12-07 15:57:41 -0600

    Added logic to track tail end and remove snake tail as it travels

--- game_logic.py
+++ game_logic.py
@@ -1,3 +1,4 @@
+from collections import deque
 from typing import Self
 from enum import Enum
 import random
@@ -18,14 +19,19 @@ class InputAction(Enum):
 class SnakeGame:
 
     def __init__(self: Self) -> Self:
-        self.rows, self.cols = (45,80)
-        self.state_arr = [[BlockState.Empty for i in range(self.cols)] for j in range(self.rows)]
-        self.head_location = (int(self.rows / 2), int(self.cols / 2))
-        self.set_block_state(self.head_location, BlockState.Snake)
-        self.place_food()
         self.score = 0
         self.is_dead = False
         self.action = InputAction.Right
+        self.rows, self.cols = (45,80)
+        self.state_arr = [[BlockState.Empty for i in range(self.cols)] for j in range(self.rows)]
+        self.head_location = (int(self.rows / 2), int(self.cols / 2))
+        self.tail_locations = deque()
+        self.tail_locations.append((self.head_location[0] + self.action.value[0] * -0, self.head_location[1] + self.action.value[1] * -0))
+        self.tail_locations.append((self.head_location[0] + self.action.value[0] * -1, self.head_location[1] + self.action.value[1] * -1))
+        self.tail_locations.append((self.head_location[0] + self.action.value[0] * -2, self.head_location[1] + self.action.value[1] * -2))
+        self.tail_locations.append((self.head_location[0] + self.action.value[0] * -3, self.head_location[1] + self.action.value[1] * -3))
+        self.set_block_state(self.head_location, BlockState.Snake)
+        self.place_food()
 
 
     def place_food(self: Self):
@@ -56,8 +62,11 @@ class SnakeGame:
             return
         if self.state_arr[new_head_x][new_head_y] == BlockState.Food:
             self.score += 1
-            self.set_block_state(self.place_food(), BlockState.Food)
+            self.place_food()
+        else:
+            self.set_block_state(self.tail_locations.popleft(), BlockState.Empty)
         self.head_location = (new_head_x, new_head_y)
+        self.tail_locations.append(self.head_location)
         self.set_block_state(self.head_location, BlockState.Snake)
         
     def get_game_state(self):
